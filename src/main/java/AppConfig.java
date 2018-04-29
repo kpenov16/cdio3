72948f881dd9f2a8fda7b3f3f18b4aa8d2647235
fagnig <fagnig@autistklassen.dk>
2018-04-29 12:11:29 +0200

    Tomcat server working properly

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,8 @@
+package main.java;
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("/rest")
+public class AppConfig extends ResourceConfig {
+}
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+package main.java;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("hello")
+public class HelloWorld {
+
+    @GET
+    public String getHello(){
+        return "Hello";
+    }
+}
