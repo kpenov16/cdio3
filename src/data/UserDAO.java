a99eb399df2be1f9b23294fea39d0e1ad5030c37
fagnig <fagnig@autistklassen.dk>
2018-04-29 16:03:32 +0200

    userDAO

---
+++ src/data/UserDAO.java
@@ -0,0 +1,54 @@
+package data;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class UserDAO {
+    List<UserDTO> userDTOList;
+
+    public List<UserDTO> getUserDTOList() {
+        return userDTOList;
+    }
+
+    public UserDTO getUser(int userId) {
+        Iterator iterator = userDTOList.iterator();
+        while (iterator.hasNext()) {
+            UserDTO tempUser = (UserDTO) iterator.next();
+            if (tempUser.getUserId() == userId) {
+                return tempUser;
+            }
+        }
+
+        return null;
+    }
+
+    public void createUser(UserDTO user) {
+        UserDTO tempUser = new UserDTO();
+        tempUser.setUserId(user.getUserId());
+        tempUser.setIni(user.getIni());
+        tempUser.setRoles(user.getRoles());
+        tempUser.setUserName(user.getUserName());
+        tempUser.setCpr(user.getCpr());
+        tempUser.setPassword(user.getPassword());
+        userDTOList.add(tempUser);
+    }
+
+    public void updateUser(UserDTO user) {
+        Iterator iterator = userDTOList.iterator();
+        while (iterator.hasNext()) {
+            UserDTO tempUser = (UserDTO) iterator.next();
+            if (tempUser.getUserId() == user.getUserId()) {
+                tempUser.setIni(user.getIni());
+                tempUser.setRoles(user.getRoles());
+                tempUser.setUserName(user.getUserName());
+                tempUser.setCpr(user.getCpr());
+                tempUser.setPassword(user.getPassword());
+                return;
+            }
+        }
+    }
+
+    public void deleteUser(int userId) {
+        userDTOList.removeIf(o -> ((UserDTO) o).getUserId() == userId);
+    }
+}
