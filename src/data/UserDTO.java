ab5da7e8c02d4603c577e89e5cb0c3a9774e6293
fagnig <fagnig@autistklassen.dk>
2018-04-29 15:45:54 +0200

    Jackson compatible UserDTO

---
+++ src/data/UserDTO.java
@@ -0,0 +1,61 @@
+package data;
+
+import java.util.List;
+
+public class UserDTO {
+    public UserDTO(){}
+    int userId;
+    String userName;
+    String ini;
+    String cpr;
+    String password;
+    List<String> roles;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getIni() {
+        return ini;
+    }
+
+    public void setIni(String ini) {
+        this.ini = ini;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
